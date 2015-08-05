d4360d09ff818a6438e0491e51d4e331214b3c87
Alexander Mezhov <as.mezhov@gmail.com>
2015-08-05 14:20:24 +0500

    Topshelf.Unix.nuspec

---
+++ .build/Build.bat
@@ -0,0 +1,5 @@
+call "%VS120COMNTOOLS%VsDevCmd.bat"
+nuget.exe restore "..\Topshelf.Unix.sln"
+msbuild "..\Topshelf.Unix.sln" /t:Clean /p:Configuration=Release 
+msbuild "..\Topshelf.Unix.sln" /p:Configuration=Release 
+nuget.exe pack "..\Topshelf.Unix\Topshelf.Unix.nuspec" -OutputDirectory "..\Topshelf.Unix\bin\Release" -symbols
